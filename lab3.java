a60f6b2d1698b60e478bf872a1cdaa874850359e
Alimo <moli.mescudi@gmail.com>
2023-10-02 23:36:25 -0400

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,1 +1,17 @@
+package lab3;
 
+
+
+public class lab3 {
+    public static void main(String[] args) {
+
+        IStack myStackInstance = new myStack();
+        IPostFixCal myPostFixCalInstance = new myPostFixCal();
+
+
+        postFixCalTesting tester = new postFixCalTesting();
+
+
+        System.out.println("Precision: " + tester.precision(myStackInstance, myPostFixCalInstance));
+    }
+}
